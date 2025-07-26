6011ca47eb438ac037c9144dedc6696737df63db
mallikarjuna45-rgb <smallikarjun713@gmail.com>
2025-07-26 15:49:52 +0530

    Time: 0 ms (100%), Space: 42 MB (75.05%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,5 +1,20 @@
 class Solution {
     public double myPow(double x, int n) {
-        return Math.pow(x,n);
+        double sum = 1;
+        long temp = n;  // Use long to avoid overflow when n = Integer.MIN_VALUE
+        if (temp < 0) {
+            temp = -temp;
+            x = 1 / x;  // Invert x for negative exponents
+        }
+        
+        while (temp != 0) {
+            if (temp % 2 == 1) {
+                sum = sum * x;
+            }
+            x = x * x;
+            temp = temp / 2;
+        }
+        
+        return sum;
     }
 }
