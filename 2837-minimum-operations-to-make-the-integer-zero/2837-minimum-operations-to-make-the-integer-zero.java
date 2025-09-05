d17f9ba7798c433e075b7399d5a9c58c6d8a8b71
mallikarjuna45-rgb <smallikarjun713@gmail.com>
2025-09-06 00:31:53 +0530

    Time: 0 ms (100%), Space: 40.9 MB (53.85%) - LeetHub

--- 2837-minimum-operations-to-make-the-integer-zero/2837-minimum-operations-to-make-the-integer-zero.java
+++ 2837-minimum-operations-to-make-the-integer-zero/2837-minimum-operations-to-make-the-integer-zero.java
@@ -1,8 +1,5 @@
 class Solution {
     public int makeTheIntegerZero(int num1, int num2) {
-        if(num1<=num2){
-            return -1;
-        }
         for(int i=1;i<=60;i++){
             long val = (long)num1 - (i * (long)num2);
             if(val<0){
